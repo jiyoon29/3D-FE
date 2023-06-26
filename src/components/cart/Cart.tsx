c1a87224ab2c6bdeea5514395b11ad4ff1bbc309
jiyoon29 <yoon__29@naver.com>
2023-06-26 14:37:30 +0900

    feat: :sparkles: 장바구니 삭제 기능 추가 및 관련 컴포넌트 변동

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,7 +3,8 @@
 import React, { useEffect, useState } from 'react'
 import CartHeader from '@/components/cart/CartHeader'
 import CartTable from '@/components/cart/CartTable'
-import { cartList } from '@/api/service/cart'
+import CartInfo from '@/components/cart/CartInfo'
+import { cartDelete, cartList } from '@/api/service/cart'
 import { CartItemProps } from '@/api/interface/cart'
 import { RootState } from '@/store/store'
 import { useSelector } from 'react-redux'
@@ -21,7 +22,7 @@ export default function Cart() {
         // 확인 필요 useSelector
         const response = await cartList(1)
         setCartItems(response.data)
-        console.log(response.data)
+        console.log('a', response.data)
       } catch (error) {
         console.log('Error fetching cart items:', error)
       }
@@ -34,24 +35,60 @@ export default function Cart() {
     if (isChecked) {
       // 선택된 카트 ID 목록에 추가
       setSelectedCartIds((prevIds) => [...prevIds, cartId])
+      console.log(selectedCartIds)
     } else {
       // 선택된 카트 ID 목록에서 제거
       setSelectedCartIds((prevIds) => prevIds.filter((id) => id !== cartId))
     }
   }
 
+  // 모든 장바구니 아이템 삭제
+  const handleDeleteAll = async () => {
+    try {
+      // 모든 장바구니 아이템 삭제
+      await cartDelete({ userId: 1, carts: cartItems.map((item) => item.cartId) })
+      setCartItems([])
+      setSelectedCartIds([])
+    } catch (error) {
+      console.log('모든 장바구니 아이템 삭제 중 오류 발생:', error)
+    }
+  }
+
+  // 선택된 장바구니 아이템 삭제
+  const handleDeleteSelected = async () => {
+    try {
+      // 선택된 장바구니 아이템 삭제
+      await cartDelete({ userId: 1, carts: selectedCartIds.map(Number) })
+      const updatedCartItems = cartItems.filter(
+        (item) => !selectedCartIds.includes(item.cartId.toString()),
+      )
+      setCartItems(updatedCartItems)
+      setSelectedCartIds([])
+    } catch (error) {
+      console.log('선택된 장바구니 아이템 삭제 중 오류 발생:', error)
+    }
+  }
+
   const selectedCount = selectedCartIds.length
 
   return (
-    <div className="w-[74.82%] px-[2.2rem] py-[4rem]">
-      <CartHeader itemCount={cartItems.length} selectedCount={selectedCount} />
-      <CartTable
-        cartItems={cartItems}
-        setCartItems={setCartItems}
-        onChecked={handleCheckboxChange}
-        selectedCartIds={selectedCartIds}
-        setSelectedCartIds={setSelectedCartIds}
-      />
-    </div>
+    <>
+      <div className="w-[74.82%] px-[2.2rem] py-[4rem]">
+        <CartHeader
+          itemCount={cartItems.length}
+          selectedCount={selectedCount}
+          onDeleteAll={handleDeleteAll}
+          onDeleteSelected={handleDeleteSelected}
+        />
+        <CartTable
+          cartItems={cartItems}
+          setCartItems={setCartItems}
+          onChecked={handleCheckboxChange}
+          selectedCartIds={selectedCartIds}
+          setSelectedCartIds={setSelectedCartIds}
+        />
+      </div>
+      <CartInfo selectedCount={selectedCount} />
+    </>
   )
 }
