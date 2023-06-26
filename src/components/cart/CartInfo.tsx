a326b28977a4df3e31462e459eb31551902ec379
jiyoon29 <yoon__29@naver.com>
2023-06-26 19:15:08 +0900

    feat: :sparkles: 체크된 결제 상품 결제 정보가져오기

--- src/components/cart/CartInfo.tsx
+++ src/components/cart/CartInfo.tsx
@@ -5,17 +5,25 @@ import Title from '../common/Title'
 
 interface CartInfoProps {
   selectedCount: number
+  selectedTotalPrice: number
+  selectedDiscountAmount: number
 }
 
-export default function CartInfo({ selectedCount }: CartInfoProps) {
+export default function CartInfo({
+  selectedCount,
+  selectedTotalPrice,
+  selectedDiscountAmount,
+}: CartInfoProps) {
+  const totalPrice = selectedTotalPrice - selectedDiscountAmount
+
   return (
-    <div className="sticky mr-[2.4rem] flex w-[23.9%] min-w-[26rem] flex-col justify-between border-l border-transparent-navy-30 px-[1.432%] py-[1.671%]">
+    <div className="sticky top-[9.6rem] mr-[2.4rem] flex h-[calc(100vh-9.6rem)] w-[23.9%] min-w-[26rem] flex-col justify-between border-l border-transparent-navy-30 px-[1.432%] py-[1.671%]">
       <div>
         <Title tit="결제정보" mb={2.4} />
         <ul className="text-ms">
           <li className="flex w-full justify-between border-b border-dashed border-transparent-navy-30 px-[0.4rem] leading-[5.6rem]">
             <p>구매할 에셋 : {selectedCount}개</p>
-            <p>{formatPrice(-25000)}</p>
+            <p>{formatPrice(selectedTotalPrice)}</p>
           </li>
           <li className="border-b border-dashed border-transparent-navy-30 text-neutral-navy-300">
             <div className="flex w-full justify-between px-[0.4rem] leading-[4.4rem]">
@@ -44,7 +52,7 @@ export default function CartInfo({ selectedCount }: CartInfoProps) {
           </li>
           <li className="flex w-full justify-between border-b border-transparent-navy-30 px-[0.4rem] leading-[5.6rem]">
             <p>자체 할인 금액</p>
-            <p>{formatPrice(-25000)}</p>
+            <p>{formatPrice(-selectedDiscountAmount)}</p>
           </li>
           <li className="flex w-full justify-between border-b border-transparent-navy-30 px-[0.4rem] leading-[5.6rem]">
             <p>총 추가 혜택</p>
@@ -55,7 +63,7 @@ export default function CartInfo({ selectedCount }: CartInfoProps) {
       <div>
         <div className="mb-[1.2rem] flex w-full justify-between text-sl leading-[5.4rem]">
           <p>결제 예정 금액</p>
-          <p>{formatPrice(154000)}</p>
+          <p>{formatPrice(totalPrice)}</p>
         </div>
         <button className="h-[4.4rem] w-full rounded-sm bg-primary-main font-semibold text-neutral-white-50">
           결제하기
